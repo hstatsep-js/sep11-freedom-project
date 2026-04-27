5ff65f786c46a85ef7035891cb3e0ed3eb22b190
Aaron Williams <aaronw136@nycstudents.net>
2026-04-27 12:40:30 -0400

    fixed the skater/Bg error

--- src/main.js
+++ src/main.js
@@ -21,7 +21,7 @@ startBtn.onclick = () => {
     scene("game", () => {
 
         const createBG = (x) => add([
-            sprite("background.png", { width: width(), height: height() }),
+            sprite("background", { width: width(), height: height() }),
             pos(x, 0),
             "bg"
         ]);
@@ -31,7 +31,7 @@ startBtn.onclick = () => {
 
 
         const player = add([
-            sprite("skater.png"),
+            sprite("skater"),
             pos(100, 200),
             area(),
             body(),
